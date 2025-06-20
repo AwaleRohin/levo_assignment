23ddb51b75065a5243c50098d36065c04a089399
rohin331 <rohinawale331@gmail.com>
2025-06-20 10:04:06 +0545

    feat: use .env in frontend

--- frontend/survey/src/api/index.js
+++ frontend/survey/src/api/index.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const apiUrl = import.meta.env.VITE_API_URL;
+
 const api = axios.create({
-  baseURL: "http://localhost:5001", // your Flask base URL
+  baseURL: apiUrl, // your Flask base URL
   headers: {
     "Content-Type": "application/json",
   },
